3f90d22fda7a1be82db3ea75c17df16c3879f7c5
faanna <sakurayu96@gmail.com>
2023-11-22 16:06:47 +0900

    javascript tabmanu js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 //변수설정
 const btns = document.querySelectorAll('ul li');
 const boxes = document.querySelectorAll('section article');
+const main = document.querySelector('main');
 
 for (let i = 0; i < btns.length; i++) {
 	btns[i].addEventListener('click', (e) => {
 		e.preventDefault();
 		active(btns, i);
 		active(boxes, i);
+
+		new Anim(main, {
+			prop: 'height',
+			value: matchHt(i),
+			duration: convertSpeed(boxes[i]),
+		});
 	});
 }
 
@@ -15,4 +22,16 @@ function active(list, index) {
 		el.classList.remove('on');
 	}
 	list[index].classList.add('on');
+}
+
+function matchHt(index) {
+	let ht = getComputedStyle(boxes[index]).height;
+	ht = parseInt(ht);
+	return ht;
+}
+
+function convertSpeed(el) {
+	let speed = getComputedStyle(el).transitionDuration;
+	speed = parseFloat(speed) * 1000;
+	return speed;
 }
